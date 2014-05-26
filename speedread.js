fb986827b34c511917b9aa9b4b860ceced6cc472
pwillia7 <ptk.williams@gmail.com>
2014-05-25 23:19:50 -0500

    revert

--- speedread.js
+++ speedread.js
@@ -98,48 +98,48 @@ $("#RBConfigurationBtn").click(function(){
 // White Thatch
 $("#RBThemeWhiteThatchBtn").click(function(){
   $("#SRinactive").removeClass("RBThemeSoftBlue RBThemeVanilla RBThemeLoudOrange").addClass("RBThemeWhiteThatch");
-  document.getElementById('RBThemeHidden').textContent = "RBThemeWhiteThatch";
+  document.getElementById('RBThemeHidden').innerText = "RBThemeWhiteThatch";
 });
 
 // Soft Blue
 $("#RBThemeSoftBlueBtn").click(function(){
    $("#SRinactive").removeClass("RBThemeWhiteThatch RBThemeLoudOrange RBThemeVanilla").addClass("RBThemeSoftBlue");
-   document.getElementById('RBThemeHidden').textContent = "RBThemeSoftBlue";
+   document.getElementById('RBThemeHidden').innerText = "RBThemeSoftBlue";
 });
 
 // Vanilla
 $("#RBThemeVanillaBtn").click(function(){
    $("#SRinactive").removeClass("RBThemeWhiteThatch RBThemeSoftBlue RBThemeLoudOrange").addClass("RBThemeVanilla");
    $("#RBMenu").removeClass("RBThemeWhiteThatch RBThemeSoftBlue RBThemeLoudOrange").addClass("RBThemeVanilla");
-   document.getElementById('RBThemeHidden').textContent = "RBThemeVanilla";
+   document.getElementById('RBThemeHidden').innerText = "RBThemeVanilla";
 });
 
 // Loud Orange
 $("#RBThemeLoudOrangeBtn").click(function(){
    $("#SRinactive").removeClass("RBThemeSoftBlue RBThemeVanilla RBThemeWhiteThatch").addClass("RBThemeLoudOrange");
-   document.getElementById('RBThemeHidden').textContent = "RBThemeLoudOrange";
+   document.getElementById('RBThemeHidden').innerText = "RBThemeLoudOrange";
 });
 
 
 // Font Size Controllers
 $("#RBFontSizeSmall").click(function(){
   $(".SRwrap").removeClass("RBFontSizeMedium RBFontSizeLarge RBFontSizeExtraLarge").addClass("RBFontSizeSmall");
-  document.getElementById('RBFontSizeHidden').textContent = "RBFontSizeSmall";
+  document.getElementById('RBFontSizeHidden').innerText = "RBFontSizeSmall";
 });
 
 $("#RBFontSizeMedium").click(function(){
   $(".SRwrap").removeClass("RBFontSizeSmall RBFontSizeLarge RBFontSizeExtraLarge").addClass("RBFontSizeMedium");
-  document.getElementById('RBFontSizeHidden').textContent = "RBFontSizeMedium";
+  document.getElementById('RBFontSizeHidden').innerText = "RBFontSizeMedium";
 });
 
 $("#RBFontSizeLarge").click(function(){
   $(".SRwrap").removeClass("RBFontSizeSmall RBFontSizeMedium RBFontSizeExtraLarge").addClass("RBFontSizeLarge");
-  document.getElementById('RBFontSizeHidden').textContent = "RBFontSizeLarge";
+  document.getElementById('RBFontSizeHidden').innerText = "RBFontSizeLarge";
 });
 
 $("#RBFontSizeExtraLarge").click(function(){
   $(".SRwrap").removeClass("RBFontSizeSmall RBFontSizeMedium RBFontSizeLarge").addClass("RBFontSizeExtraLarge");
-  document.getElementById('RBFontSizeHidden').textContent = "RBFontSizeExtraLarge";
+  document.getElementById('RBFontSizeHidden').innerText = "RBFontSizeExtraLarge";
 });
 
   //start readability alg--
@@ -184,7 +184,7 @@ $("#RBFontSizeExtraLarge").click(function(){
       }
 
       // Add a point for the paragraph found
-      if(gettextContent(allParagraphs[j]).length > 10)
+      if(getInnerText(allParagraphs[j]).length > 10)
         parentNode.readability.contentScore++;
 
       // Add points for any commas within this paragraph
@@ -206,13 +206,13 @@ $("#RBFontSizeExtraLarge").click(function(){
 
     
     
-    return topDiv.textContent.split(/[\r\n ]/).filter(function(v){return v!==''});
+    return topDiv.innerText.split(/[\r\n ]/).filter(function(v){return v!==''});
   }
 
   // Get the inner text of a node - cross browser compatibly.
-  function gettextContent(e) {
+  function getInnerText(e) {
     if (navigator.appName == "Microsoft Internet Explorer")
-      return e.textContent;
+      return e.innerText;
     else
       return e.textContent;
   }
@@ -220,7 +220,7 @@ $("#RBFontSizeExtraLarge").click(function(){
   // Get character count
   function getCharCount ( e,s ) {
       s = s || ",";
-    return gettextContent(e).split(s).length;
+    return getInnerText(e).split(s).length;
   }
   // end readability code
 
@@ -239,8 +239,8 @@ function speedRead1() {
     var middleChar;
     var startOfWord;
     var endOfWord;
-    var theme = document.getElementById('RBThemeHidden').textContent;
-    var fontSize = document.getElementById('RBFontSizeHidden').textContent;
+    var theme = document.getElementById('RBThemeHidden').innerText;
+    var fontSize = document.getElementById('RBFontSizeHidden').innerText;
 
     limit = words.length;
     var halflength = words[i].length/2;
